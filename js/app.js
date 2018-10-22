9675f03d06ea9d503966237b7d49b5049d912516
andrewquamme <andrewquamme@gmail.com>
2018-10-22 16:46:37 -0700

    fixes html, styles css, codes js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@
 
 // Create array to store objects
 var allItems = [];
+var displayedNow = [];
+// Get <img> elements from DOM
+var itemOne = document.getElementById('item-one');
+var itemTwo = document.getElementById('item-two');
+var itemThree = document.getElementById('item-three');
+var itemLocations = [];
+itemLocations[0] = itemOne;
+itemLocations[1] = itemTwo;
+itemLocations[2] = itemThree;
 
 // Object constructor
 function CatalogItem (name, filepath) {
@@ -24,7 +33,7 @@ new CatalogItem('cthulhu', 'img/cthulhu.jpg');
 new CatalogItem('dog-duck', 'img/dog-duck.jpg');
 new CatalogItem('dragon', 'img/dragon.jpg');
 new CatalogItem('pen', 'img/pen.jpg');
-new CatalogItem('pet-sweep', 'pet-sweep.jpg');
+new CatalogItem('pet-sweep', 'img/pet-sweep.jpg');
 new CatalogItem('scissors', 'img/scissors.jpg');
 new CatalogItem('shark', 'img/shark.jpg');
 new CatalogItem('sweep', 'img/sweep.png');
@@ -34,5 +43,23 @@ new CatalogItem('usb', 'img/usb.gif');
 new CatalogItem('water-can', 'img/water-can.jpg');
 new CatalogItem('wine-glass', 'img/wine-glass.jpg');
 
+function getRandomItem() {
+  var idx = Math.floor(Math.random() * allItems.length);
+  return allItems[idx];
+}
+
+function checkDuplicate() {
+
+}
+
+function displayItems() {
+  for (var i = 0; i < itemLocations.length; i ++) {
+    var item = getRandomItem();
+    itemLocations[i].src = item.filepath;
+    itemLocations[i].title = item.name;
+    itemLocations[i].alt = item.name;
+  }
+}
 
 console.table(allItems);
+displayItems();
