28c5919800629192715edafc38e0ab65bd555fcc
andrewquamme <andrewquamme@gmail.com>
2018-10-23 10:21:50 -0700

    renames image files to automate object creation

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 // *********************
 // GLOBALS
+// *********************
 
 var allItems = [];
 var displayedItems = [];
@@ -17,34 +18,19 @@ var right = document.getElementById('item-three');
 // OBJECTS
 // *********************
 
-function CatalogItem (name, filepath) {
+function CatalogItem (name) {
   this.name = name;
-  this.filepath = filepath;
+  this.filepath = `img/${name}.jpg`;
   this.views = 0;
   this.clicks = 0;
   allItems.push(this);
 }
 
-new CatalogItem('bag', 'img/bag.jpg');
-new CatalogItem('banana', 'img/banana.jpg');
-new CatalogItem('bathroom', 'img/bathroom.jpg');
-new CatalogItem('boots', 'img/boots.jpg');
-new CatalogItem('breakfast', 'img/breakfast.jpg');
-new CatalogItem('bubblegum', 'img/bubblegum.jpg');
-new CatalogItem('chair', 'img/chair.jpg');
-new CatalogItem('cthulhu', 'img/cthulhu.jpg');
-new CatalogItem('dog-duck', 'img/dog-duck.jpg');
-new CatalogItem('dragon', 'img/dragon.jpg');
-new CatalogItem('pen', 'img/pen.jpg');
-new CatalogItem('pet-sweep', 'img/pet-sweep.jpg');
-new CatalogItem('scissors', 'img/scissors.jpg');
-new CatalogItem('shark', 'img/shark.jpg');
-new CatalogItem('sweep', 'img/sweep.png');
-new CatalogItem('tauntaun', 'img/tauntaun.jpg');
-new CatalogItem('unicorn', 'img/unicorn.jpg');
-new CatalogItem('usb', 'img/usb.gif');
-new CatalogItem('water-can', 'img/water-can.jpg');
-new CatalogItem('wine-glass', 'img/wine-glass.jpg');
+var itemNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+for (var i = 0; i < itemNames.length; i ++ ) {
+  new CatalogItem(itemNames[i]);
+}
 
 // *********************
 // FUNCTIONS
