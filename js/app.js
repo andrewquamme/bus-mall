0747a353db9670b8c718c72aa8e4bb3acbad5eef
andrewquamme <andrewquamme@gmail.com>
2018-10-24 08:39:57 -0700

    adds views to chart

--- js/app.js
+++ js/app.js
@@ -12,8 +12,11 @@ var itemContainer = document.getElementById('item-container');
 var left = document.getElementById('left-item');
 var center = document.getElementById('center-item');
 var right = document.getElementById('right-item');
-var itemList = document.getElementById('item-list');
-var ctx = document.getElementById('chart').getContext('2d');
+// var itemList = document.getElementById('item-list');
+
+var chartLabels = [];
+var chartVotes = [];
+var chartViews = [];
 
 // +++++++++++++++++++++++++++++++++
 // OBJECTS
@@ -61,7 +64,7 @@ function getThreeUniques () {
     thirdNum = (randomNum());
   }
   output.push(thirdNum);
-  console.log(output);
+
   return output;
 }
 
@@ -94,15 +97,7 @@ function handleItemClick (event) {
   totalVotes++;
 
   if (totalVotes === 25) {
-    disableAndHideImages();
-
-    // var votes = 0;
-    // for (i = 0; i < allItems.length; i++) {
-    //   votes += allItems[i].clicks;
-    // }
-    // console.log(votes);
-
-    return showList();
+    return showChart();
   }
   displayItems();
 }
@@ -110,15 +105,79 @@ function handleItemClick (event) {
 function disableAndHideImages() {
   itemContainer.removeEventListener('click', handleItemClick);
   alert('Thank you for your input!') ;
-  console.table(allItems);
-  itemContainer.textContent = '';
+  // console.table(allItems);
+  itemContainer.innerHTML = '';
 }
 
-function showList() {
+// function showList() {
+//   for (var i = 0; i < allItems.length; i ++) {
+//     var liEl = document.createElement('li');
+//     liEl.textContent = `${allItems[i].name} has ${allItems[i].views} views and ${allItems[i].clicks} votes`;
+//     itemList.appendChild(liEl);
+//   }
+// }
+
+function showChart() {
+  disableAndHideImages();
+  makeChartArrays();
+  var ctx = document.getElementById('chart').getContext('2d');
+
+  var data = {
+    labels: chartLabels, // labels array declared earlier
+    datasets: [{
+      label: 'Votes',
+      data: chartVotes, // votes array declared earlier
+      backgroundColor: [
+        'rgba(255, 0, 0, 0.2)', 'rgba(255, 165, 0, 0.2)', 'rgba(255, 255, 0, 0.2)', 'rgba(0, 255, 0, 0.2)', 'rgba(0, 0, 255, 0.2)',
+        'rgba(255, 0, 0, 0.2)', 'rgba(255, 165, 0, 0.2)', 'rgba(255, 255, 0, 0.2)', 'rgba(0, 255, 0, 0.2)', 'rgba(0, 0, 255, 0.2)',
+        'rgba(255, 0, 0, 0.2)', 'rgba(255, 165, 0, 0.2)', 'rgba(255, 255, 0, 0.2)', 'rgba(0, 255, 0, 0.2)', 'rgba(0, 0, 255, 0.2)',
+        'rgba(255, 0, 0, 0.2)', 'rgba(255, 165, 0, 0.2)', 'rgba(255, 255, 0, 0.2)', 'rgba(0, 255, 0, 0.2)', 'rgba(0, 0, 255, 0.2)',
+        'rgba(255, 0, 0, 0.2)', 'rgba(255, 165, 0, 0.2)', 'rgba(255, 255, 0, 0.2)', 'rgba(0, 255, 0, 0.2)', 'rgba(0, 0, 255, 0.2)'],
+      borderWidth: 1,
+      borderColor: [
+        'red', 'orange', 'yellow', 'green', 'blue',
+        'red', 'orange', 'yellow', 'green', 'blue',
+        'red', 'orange', 'yellow', 'green', 'blue',
+        'red', 'orange', 'yellow', 'green', 'blue',
+        'red', 'orange', 'yellow', 'green', 'blue',]
+    },{
+      label: 'Views',
+      data: chartViews, // views array declared earlier
+      backgroundColor: [
+        'rgba(0, 0, 255, 0.2)'],
+      borderWidth: 1,
+      borderColor: [
+        'blue']
+    }]
+  }
+
+  var options = {
+    responsive: false,
+    animation: {
+      duration: 2000,
+      easing: 'linear'
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+
+  var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: data,
+    options: options,
+  });
+}
+
+function makeChartArrays() {
   for (var i = 0; i < allItems.length; i ++) {
-    var liEl = document.createElement('li');
-    liEl.textContent = `${allItems[i].name} has ${allItems[i].views} views and ${allItems[i].clicks} votes`;
-    itemList.appendChild(liEl);
+    chartLabels.push(allItems[i].name);
+    chartVotes.push(allItems[i].clicks);
+    chartViews.push(allItems[i].views);
   }
 }
 
